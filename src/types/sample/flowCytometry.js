3b276f465128c968d4eadad4a3c4a70e2607cbed
josoriom <javier.osorio@correounivalle.edu.co>
2020-12-03 17:02:15 -0500

    feat: add flowCytometry

---
+++ src/types/sample/flowCytometry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+  jpath: ['spectra', 'flowCytometry'],
+  find: common.basenameFind,
+  getProperty: common.getTargetProperty,
+};
