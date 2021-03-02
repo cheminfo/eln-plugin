54733094230dea81e80a702de4eeef7b1baba1ec
Luc Patiny <luc@patiny.com>
2021-03-02 08:00:10 +0100

    feat: add hgPorosimetry

---
+++ src/types/sample/hgPorosimetry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const common = require('../common');
+
+module.exports = {
+  jpath: ['spectra', 'hgPorosimetry'],
+  find: common.basenameFind,
+  getProperty: common.getTargetProperty
+};
