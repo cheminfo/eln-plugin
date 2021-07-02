1e791b9cecef939458244d248dcf107f6f2e11c1
Luc Patiny <luc@patiny.com>
2021-07-02 09:18:43 +0200

    Revert "feat: add bet"
    This reverts commit 2ef9a3db692bb306c81797b22d00e0c390426504.

--- src/types/sample/bet.js
+++
@@ -1,9 +0,0 @@
-import common from '../common';
-
-/** Brunauer-Emmett-Teller (BET) surface area analysis */
-export default {
-  jpath: ['spectra', 'bet'],
-  find: common.basenameFind,
-  getProperty: common.getTargetProperty,
-  process: common.getMetaFromJcamp,
-};
