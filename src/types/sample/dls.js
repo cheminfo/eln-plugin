a3b994ccb747db9bd73f0bb04dbcbf2e74302897
Luc Patiny <luc@patiny.com>
2021-12-08 07:27:39 +0100

    fix: dls jpath

--- src/types/sample/dls.js
+++ src/types/sample/dls.js
@@ -1,7 +1,7 @@
 import common from '../common';
 
 export default {
-  jpath: ['spectra', 'icp'],
+  jpath: ['spectra', 'dls'],
   find: common.basenameFind,
   getProperty: common.getTargetProperty,
   process: common.getMetaFromJcamp,
